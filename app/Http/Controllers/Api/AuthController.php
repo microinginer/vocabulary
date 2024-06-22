b94ad9981402381f927b5c3764c6b4b54aecc049
inginer <ruslanmadatov@yandex.ru>
2024-06-22 23:33:02 +0500

    added create challeng on sign up

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserChallenge;
+use App\Models\Challenge;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +13,7 @@ use Google\Client as GoogleClient;
 
 class AuthController extends Controller
 {
-    public function handleGoogleCallback(Request $request)
+    public function handleGoogleCallback(Request $request): \Illuminate\Http\JsonResponse
     {
         $client = new GoogleClient(['client_id' => env('GOOGLE_CLIENT_ID')]);  // Укажите ваш Google Client ID
         $payload = $client->verifyIdToken($request->token);
@@ -27,13 +29,41 @@ class AuthController extends Controller
 
             $token = $user->createToken('authToken')->plainTextToken;
 
+            if ($user->wasRecentlyCreated) {
+                $this->createInitialChallenges($user);
+            }
+
             return response()->json(['token' => $token], 200);
         } else {
             return response()->json(['error' => 'Invalid token'], 401);
         }
     }
 
-    public function logout(Request $request)
+    private function createInitialChallenges(User $user): void
+    {
+        $dailyChallenges = Challenge::where('type', 'daily')->inRandomOrder()->take(1)->get();
+        $weeklyChallenges = Challenge::where('type', 'weekly')->inRandomOrder()->take(1)->get();
+
+        foreach ($dailyChallenges as $challenge) {
+            UserChallenge::create([
+                'user_id' => $user->id,
+                'challenge_id' => $challenge->id,
+                'progress' => 0,
+                'completed' => 0,
+            ]);
+        }
+
+        foreach ($weeklyChallenges as $challenge) {
+            UserChallenge::create([
+                'user_id' => $user->id,
+                'challenge_id' => $challenge->id,
+                'progress' => 0,
+                'completed' => 0,
+            ]);
+        }
+    }
+
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
     {
         $request->user()->currentAccessToken()->delete();
         return response()->json(['message' => 'Logged out successfully'], 200);
